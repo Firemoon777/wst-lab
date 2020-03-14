6288f5ba66691e2fe97bc599cdf79c2cc95ed3ca
Vladimir Turov <firemoon@icloud.com>
2020-03-14 21:05:32 +0300

    lab4 finished

--- wst-lab-4/client/src/main/java/org/example/wst/client/WebServiceClient.java
+++ wst-lab-4/client/src/main/java/org/example/wst/client/WebServiceClient.java
@@ -4,6 +4,9 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.GenericType;
 import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
 import org.example.wst.entity.Cat;
 
 import javax.ws.rs.core.MediaType;
